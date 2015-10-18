24a97049da4f423fda592649d04aeb1d41581718
timserck <timserck@gmail>
2015-10-18 21:17:23 +0200

    remise

---
+++ fichiers/app/pages/inscription.php
@@ -0,0 +1,19 @@
+            <main class="container espace_connexion">
+
+                <section>
+                    
+                    <form class="form_connexion" method="post">
+                        
+                        <input class="connexion_mail" type="email" name="mail" placeholder="Email">
+
+                        <input class="connexion_lock" type="text" name="password" placeholder="Mot de passe">
+
+                        <a class="form_submit_test link_connect" href="">M'inscrire</a>
+                        
+                        <a class="form_no_compte link_inscription" href="">J'ai déjà un compte</a>
+
+                    </form>
+
+                </section>
+           
+            </main>
