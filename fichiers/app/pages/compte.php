fbf14d50bebe39c0221e9842491627a29db8c21a
timserck <timserck@gmail>
2015-10-18 21:14:39 +0200

    remise

---
+++ fichiers/app/pages/compte.php
@@ -0,0 +1,52 @@
+
+
+            <mainclass="compte_overflow">
+
+                <section>
+
+                    <div class="top_profile">
+                  
+                    <img class="compte_img" src="_img/profil.png" alt="photo de profile">
+
+                    <h2 class="compte_title">Rémy Martiat</h2>
+
+                    <p class="compte_abo">Abonnement: <span class="compte_span">86 </span>jours restants</p>
+
+                    </div>
+
+                    <ul class="compte_list container">
+
+                        <li class="compte_stat">
+
+                            <img class="compte_icone" src="_img/km_velo.svg" alt="icone kilomètres parcourus">
+
+                            <p class="compte_stat_info">285<span class="compte_stat_span">km</span></p>
+
+                        </li>
+
+                        <li class="compte_stat">
+
+                            <img class="compte_icone" src="_img/temps_velo.svg" alt="icone temps passé à vélo">
+
+                            <p class="compte_stat_info">150<span class="compte_stat_span">h</span></p>
+
+                        </li>
+
+                        <li class="compte_stat">
+
+                            <img class="compte_icone" src="_img/calorie_velo.svg" alt="icone calories brulées à vélo">
+
+                            <p class="compte_stat_info">5400<span class="compte_stat_span">cal</span></p>
+
+                        </li>
+
+
+                    </ul>
+
+                    <a class="compte_logout" href="index.php">Déconnexion</a>
+                  
+                </section>
+           
+            </main>
+            
+    
---
+++ fichiers/app/pages/header.php
@@ -0,0 +1,16 @@
+ <header>
+    <ul class="header_list cf">
+         <li>
+            <a class="header_back link_back" href="">Back</a>
+
+         </li>
+          <li>
+            <h1 class="headerTitle">Libia Vélo</h1>
+        </li>
+         <li>
+            <a class="header_link_profil link_compte" href="">Mon compte</a>
+        </li>
+     </ul>
+</header>
+
+
