ba042c9abd388c9bb307637fa68f8c6a74acdb04
timserck <timserck@gmail>
2015-10-18 21:14:29 +0200

    remise

---
+++ fichiers/app/pages/accueil.php
@@ -0,0 +1,33 @@
+
+
+            <main class="accueil_main">
+
+                <section class="accueil_section">
+                    
+                   <ul class="accueil_list">
+
+                    <li>
+
+                        <a class="accueil_button link_map" href="">Réserver un vélo</a>
+
+                    </li>
+
+                    <li>
+
+                        <a class="accueil_button link_borne" href="">Scanner mon abonnement</a>
+
+
+                    </li>
+
+
+                   </ul>
+
+                </section>
+           
+            </main>
+            
+
+        
+    
+
+</html>
