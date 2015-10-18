fbc96367ee33e8f62f63d6a5c39a1dbd9c51081a
timserck <timserck@gmail>
2015-10-18 21:14:56 +0200

    remise

---
+++ fichiers/app/pages/index.php
@@ -0,0 +1,23 @@
+
+
+            <main class="container espace">
+
+                <section>
+                    
+                    <form class="form_connexion" method="post">
+                        
+                        <input class="connexion_mail" type="email" name="mail" placeholder="Email">
+
+                        <input class="connexion_lock" type="text" name="password" placeholder="Mot de passe">
+
+
+                        <a class="form_submit_test link_connect" href="">Me connecter</a>
+                        
+                        <a class="form_no_compte link_inscription" href="">Je n'ai pas de compte</a>
+
+                    </form>
+
+                </section>
+           
+            </main>
+            
